8d07d45093c4f359d10a6b8563ef64884acf0f7e
anilsaka <anilsaka.ggs@hotmail.com>
2023-06-01 15:06:26 +0300

    update

--- routes/web.php
+++ routes/web.php
@@ -25,14 +25,10 @@ Route::post('/file-import', [ProductController::class, 'import'])->name('file-im
 
 Route::resource('bids' , BidController::class);
 
-// Route::get('/register', function () {
-//     return redirect('/');
-// });
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
 
-// Route::middleware('auth')->group(function () {
-//     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
-//     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
-//     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-//     });
-
-// require __DIR__.'/auth.php';
+require __DIR__.'/auth.php';
